0b595e378fe81577f0d5cb01fb3b1b0e14404e5d
Luke Bowen <lbowen3@students.avc.edu>
2023-10-23 08:51:13 -0700

    Fixed typo

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def main():
 
     celsius = float(input('Enter temperature in celsuius: '))
     fahrenheit = (9/5 * celsius) + 32
-    print(f'The temperature in farenheit is: {fahrenheit:.2f}')
+    print(f'The temperature in fahrenheit is: {fahrenheit:.2f}')
 
     """
     ########################################
